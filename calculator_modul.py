908d87b97d07a6493e7dda08a60005df678a48fe
Hit Javiya <hitjaviya1227@gmail.com>
2023-02-16 09:03:26 +0530

    first

---
+++ calculator_modul.py
@@ -0,0 +1,2 @@
+opration = lambda a,b:   print(a+b) if sign == "+" else print(a-b)  if sign=="-" else print(a/b)  if sign=="/" else print(a*b) 
+sign=input("Enter opration : ")
---
+++ knapsak.py
@@ -0,0 +1,3 @@
+cap=int(input("enter capacity : "))
+n=int(input("enter size : "))
+value=[ int(input("enter value : ")) for i in range(0,n)]
